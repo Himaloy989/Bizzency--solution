94672dc0859eb42de43f5b8e754affd514eefae3
Himaloy Datta. (Himu) <121860658+Himaloy989@users.noreply.github.com>
2023-04-15 13:56:00 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.success-slider').slick({
+        prevArrow:'<i class="slider-arrow fa-solid fa-chevron-left"></i>',
+        nextArrow:'<i class="slider-arrow fa-solid fa-chevron-right"></i>'
+    })
+})
